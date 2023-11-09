034f2185cb0668358bcac4f0d44cd2eadf907b69
CosmicMusashi <joshua.y1791@gmail.com>
2023-11-08 20:52:03 -0500

    add custom elements

--- customelements.js
+++ customelements.js
@@ -1,7 +1,23 @@
-class JoshElement extends HtmlElement{
+class JoshElement extends HTMLElement{
     connectedCallback(){
         this.innerHTML = "Josh was here";
     }
 }
 
 customElements.define("x-josh", JoshElement);
+
+class CenterElement extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = `<div "style=text-align:center">${this.innerHTML}</div>`;
+    }
+}
+
+customElements.define("x-center", CenterElement);
+
+class YearElement extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = new Date().getFullYear();
+    }
+}
+
+customElements.define("x-fullyear", YearElement);
